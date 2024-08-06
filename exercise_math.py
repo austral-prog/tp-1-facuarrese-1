e7b3da6c9cdbf6d0b044de6d3019abe3571e7627
facuarrese <162734700+facuarrese@users.noreply.github.com>
2024-08-06 16:33:18 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    print (a+b)
+    print (a-b)
+    print (a*b)
+    print ((a+b)/2)
+    print (a//b)
+    print (a%b)
+    print (a/b)
+    #64
+    #50
+    #399
+    #32.0
+    #8
+    #1
+    #8.142857142857142    
